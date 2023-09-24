31528899c35fcba825145f8646a356c474e35d57
stufffy <jonsnow1996@skiff.com>
2023-09-24 19:28:17 +0800

    idiots commiting stuff

--- learnyounode/idiot2.js
+++ learnyounode/idiot2.js
@@ -1,9 +1,11 @@
 function innerFunction (name1, name2, callback) {
     
     const result1 = name1 + ' and ' + name2 + ' are idiots.'
-    const result2 = name2 + ' and ' + name1 + ' are idiots.'
+    const result2 = name2 + ' and ' + name1 + ' are idiots!'
 
     callback(null, result1, result2)
 }
 
 module.exports = innerFunction
+
+// lmao idiots lol
